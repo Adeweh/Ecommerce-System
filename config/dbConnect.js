8927c42798476a79eb5c45eb1ef929a385f59466
Adeweh <dorcasabang@gmail.com>
2023-05-25 10:16:18 +0100

    Database connected|

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,5 +1,13 @@
 const { default: mongoose } = require("mongoose")
 
 const dbConnect = () =>{
-    const conn = mongoose.connect("")
-}
+    try{
+        const conn = mongoose.connect("mongodb://localhost:27017/ecommerce");
+        console.log("Database connected successfully");
+    }
+    catch(error){
+        console.log("Database error");
+    }
+};
+
+module.exports = dbConnect;
