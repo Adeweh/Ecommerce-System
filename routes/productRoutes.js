fbe460dd20c47a7554c399be5e3e9301006d46d1
Adeweh <dorcasabang@gmail.com>
2023-06-10 14:52:40 +0100

    wishlist implementation

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,11 +1,14 @@
 const express = require("express");
-const { createProduct, getProduct, getAProduct, getAllProduct, updateProduct, deleteProduct } = require("../controller/productController");
+const { createProduct, getAProduct, getAllProduct, updateProduct, deleteProduct, addToWishList } = require("../controller/productController");
 const {isAdmin, authMiddleware} = require("../middlewares/authMiddleware")
 const router = express.Router();
 
 
 router.post("/", authMiddleware, isAdmin, createProduct);
 router.get("/:id", getAProduct);
+router.put("/wishlist", authMiddleware, addToWishList);
+
+
 router.put("/:id", authMiddleware, isAdmin, updateProduct);
 router.delete("/:id", authMiddleware, isAdmin, deleteProduct);
 router.get("/", getAllProduct);
